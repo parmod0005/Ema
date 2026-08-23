66ff72d0e25ae0830cc9055b1f1a907d6e7ff97e
parmod0005 <69755180+parmod0005@users.noreply.github.com>
2026-08-23 12:46:21 +0530

    Version full VARDHANI product build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.parmod.ema"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-tick"
+        versionCode = 4
+        versionName = "1.0.0-full"
     }
 
     buildFeatures {
